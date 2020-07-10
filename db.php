768ca5dd43da0312f73bdb823c02bc635dc82c90
validName <vrsevertech@gmail.com>
2020-07-10 09:48:26 +0300

    проверка телефона на число

--- db.php
+++ db.php
@@ -276,6 +276,7 @@ class orderProcessing extends db
 
     function go($eventId, $phone, $name, $order)
     {
+        if (!is_numeric($phone)) exit('не цифры');
         $order = explode(',', $order);
         if (!count($order)) exit('пустой заказ');
         $in = str_repeat('?,', count($order) - 1) . '?';
