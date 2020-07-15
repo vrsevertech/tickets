e2bf537ffae45686fc34c07ba4590e5c849ed4c4
validName <vrsevertech@gmail.com>
2020-07-15 14:50:36 +0300

    несколько событий

--- index.php
+++ index.php
@@ -5,10 +5,34 @@ if(isset($_SESSION['ok'])) {
         "<br>перезвоним: " . $_SESSION['ok']['phone'] . 
         "<br>номер заказа: " . $_SESSION['ok']['orderId'] . 
         "<br>зарезервировано: " . $_SESSION['ok']['order'] . 
+        "<br>событие: " . $_SESSION['ok']['eventId'] .
         "<br>";
     unset($_SESSION['ok']);
 } 
+
+
+
+require 'db_connection.php';
+$pdo = new PDO(
+    "mysql:host=$host;dbname=$dbname;charset=utf8", 
+    $login, $password, [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false, 
+]);
+
+$events = $pdo->prepare("SELECT * FROM events");
+$events->execute();
+$events = $events->fetchAll();
+
+foreach ($events as $event) {
+	echo '<a href="./';
+	echo $event['eventId'];
+	echo '">';
+	echo $event['eventName'] . ' ' . $event['eventDate'];
+	echo '</a>';
+	echo '<br>';
+}
 ?>
-<a href="./1">тестовое событие</a>
 <br><br><br><br><br><br><br>
 <?php //phpinfo(); ?>
