08399e76d15e872e979da441ec4661501f7fb160
validName <vrsevertech@gmail.com>
2021-03-24 16:15:32 +0200

    забыл указать расширение..

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ foreach ($events as $event) {
 }
 ?>
 <br><br><br><a href="./result">Просмотреть список зарезервировавших</a>
-<br><a href="./clean">Очистить список зарезервировавших</a>
+<br><a href="./clean.php">Очистить список зарезервировавших</a>
 
 <br><br><br><br><br>
 <?php //phpinfo(); ?>
